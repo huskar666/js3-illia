b1b9d367eb9b496a39534ddb07c5b0e16e8ecd87
huskar666 <148646923+huskar666@users.noreply.github.com>
2024-09-20 21:54:20 +0300

    code

---
+++ script.js
@@ -0,0 +1,22 @@
+const result = 5 + 5 + '5';
+console.log(typeof result);
+
+
+let email = 'illia.nikolaienko10@gmail.com';
+const emailSobachka = email.includes('@');
+const emailLength = email.length;
+
+console.log(emailSobachka);
+console.log(emailLength)
+
+
+let a = 'My ';
+let b = 'name ';
+let c = 'is ';
+const fullName = a + b + c + 'Viktor';
+console.log(fullName)
+
+
+let userName = 'Illia';
+let payment = 100;
+alert('Дякуємо, ' + userName + '! ' + 'До сплати ' + payment + ' гривень');
